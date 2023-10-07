3ba10b04f71ff0002dbe252ae30ccaa25934076b
Vyron Arediano <cedierickvyron.arediano@playerelite.com.au>
2023-10-07 22:44:50 +0800

    chore: Added badge view in reminder list item

--- shared/src/commonMain/kotlin/com/petluu/app/feature_pets/presentation/components/ReminderListItem.kt
+++ shared/src/commonMain/kotlin/com/petluu/app/feature_pets/presentation/components/ReminderListItem.kt
@@ -1,19 +1,25 @@
 package com.petluu.app.feature_pets.presentation.components
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.petluu.app.core.presentation.util.Dimens
@@ -31,9 +37,9 @@ fun ReminderListItem(
             .fillMaxWidth()
             .padding(vertical = Padding.Vertical.SM, horizontal = Padding.Horizontal.MD),
         shape = RoundedCornerShape(Dimens.RoundedCornerRadius.large),
- /*       elevation = CardDefaults.cardElevation(
-            defaultElevation = Dimens.Elevation.petListItemCardElevation
-        ),*/
+        /*       elevation = CardDefaults.cardElevation(
+                   defaultElevation = Dimens.Elevation.petListItemCardElevation
+               ),*/
         border = BorderStroke(1.dp, MaterialTheme.colorScheme.outlineVariant),
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.background
@@ -44,35 +50,65 @@ fun ReminderListItem(
                 .fillMaxWidth()
                 .height(Dimens.Card.reminderCardHeight)
         ) {
-            Column(
-                modifier = Modifier.padding(Padding.All.MD),
-                verticalArrangement = Arrangement.spacedBy(Spacing.Vertical.XS)
-            ) {
-                Text(
-                    text = reminder.title,
-                    style = MaterialTheme.typography.titleMedium,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
-                )
+            Row {
+                Column(
+                    modifier = Modifier.weight(1f).padding(Padding.All.MD),
+                    verticalArrangement = Arrangement.spacedBy(Spacing.Vertical.XS)
+                ) {
+                    Text(
+                        text = reminder.title,
+                        style = MaterialTheme.typography.titleMedium,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer
+                    )
 
-                Text(
-                    text = "${reminder.petName} | ${reminder.recordType}",
-                    style = MaterialTheme.typography.labelMedium,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    color = MaterialTheme.colorScheme.outline
-                )
+                    Text(
+                        text = "${reminder.petName} | ${reminder.recordType}",
+                        style = MaterialTheme.typography.labelMedium,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        color = MaterialTheme.colorScheme.outline
+                    )
 
-                // TODO replace it with reminder.date
-                Text(
-                    text = "Sept 20, 2023 at 12:15 PM",
-                    style = MaterialTheme.typography.labelMedium,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
-                )
+                    // TODO replace it with reminder.date
+                    Text(
+                        text = "Sept 20, 2023 at 12:15 PM",
+                        style = MaterialTheme.typography.labelMedium,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer
+                    )
+                }
+
+                Box(
+                    modifier = Modifier
+                        .padding(end = Padding.Horizontal.MD)
+                        .align(Alignment.CenterVertically)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .background(
+                                color = MaterialTheme.colorScheme.tertiaryContainer,
+                                shape = RoundedCornerShape(Dimens.RoundedCornerRadius.large)
+                            ),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(
+                                vertical = Padding.Horizontal.SM,
+                                horizontal = Padding.Horizontal.MD
+                            ),
+                            text = "Next week",
+                            style = MaterialTheme.typography.labelMedium,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            color = MaterialTheme.colorScheme.onPrimaryContainer
+                        )
+                    }
+                }
             }
+
         }
     }
 }
